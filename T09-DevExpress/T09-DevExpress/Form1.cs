888c914bb9da85f2b0dc0b0780e06485a26c2df5
lglong519 <769141564@qq.com>
2019-06-07 02:32:04 +0800

    devexpress

---
+++ T09-DevExpress/T09-DevExpress/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Helpers;
+
+
+namespace T09_DevExpress
+{
+    public partial class Form1 : RibbonForm
+    {
+        public Form1()
+        {
+            ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.EngineOrDesktop);
+            ESRI.ArcGIS.RuntimeManager.BindLicense(ESRI.ArcGIS.ProductCode.Engine);
+            InitializeComponent();
+            InitSkinGallery();
+        }
+        void InitSkinGallery()
+        {
+            //SkinHelper.InitSkinGallery(rgbiSkins, true);
+            ribbonControl.SelectedPageChanged += new EventHandler((object sender, EventArgs e) => {
+                Console.WriteLine(ribbonControl.SelectedPage.Text);
+                Console.WriteLine(ribbonControl.SelectedPage.Name);
+                Console.WriteLine(ribbonControl.SelectedPage.PageIndex);
+                Console.WriteLine(ribbonControl.SelectedPage.Visible);
+            });
+        }
+
+        private void iOpen_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "打开地图文档文件";
+            openFileDialog.Filter = "map documents(*.mxd)|*.mxd";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                axMapControl1.LoadMxFile(filePath, 0, Type.Missing);
+                axMapControl1.Extent = axMapControl1.FullExtent;
+            }
+        }
+
+        private void barButtonItem1_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Console.WriteLine(xtraTabControl1.TabIndex.ToString());
+            Console.WriteLine(xtraTabControl1.SelectedTabPageIndex.ToString());
+        }
+    }
+}
---
+++ T09-DevExpress/T09-DevExpress/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("T09-DevExpress")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("T09-DevExpress")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e0b4d5b-fc56-497f-ad54-665a7a50e3d7")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
